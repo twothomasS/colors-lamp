ecadc373d67630334665426b27990df840815871
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 13:16:16 -0400

    Added functions to properly turn results to jsons and return to callee

--- api/SearchColor.php
+++ api/SearchColor.php
@@ -13,6 +13,28 @@
 	} 
 	else
 	{
-  }
+  	}
 
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
 ?>
