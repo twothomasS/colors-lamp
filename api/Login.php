9dd9ada48b1f0b7f4d5064bcd8c3f19e0262d1d4
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 13:07:14 -0400

    Added Feature to decode key.json and json sent by frontend, and make a connection to the server

--- api/Login.php
+++ api/Login.php
@@ -1,1 +1,21 @@
+<?php
 
+	$keyjson = file_get_contents('key.json');
+	$keydata = json_decode($keyjson, true);
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	$conn = new mysqli($keydata['host'], $keydata['user'], $keydata['password'], $keydata['db']);
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+    
+  }
+
+?>
