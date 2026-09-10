7481268444a8b3334be8afc2b5404c530c1eb20f
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 14:01:25 -0400

    Add cookie functionality

--- public/js/code.js
+++ public/js/code.js
@@ -1,1 +1,47 @@
+const urlBase = 'http:YOURDOMAIN/api'; //CHANGE TO THE DOMAIN NAME OF YOUR WEBSITE
+const extension = 'php';
 
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
