238df76e1684d35da6f46432e97600c0e3b70e11
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 13:15:16 -0400

    Added the ability to get key information from json, decode json given by callee, and make a connection to the database

--- api/SearchColor.php
+++ api/SearchColor.php
@@ -1,1 +1,18 @@
+<?php
+	$keyjson = file_get_contents('key.json');
+	$keydata = json_decode($keyjson, true);
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
 
+	$conn = new mysqli($keydata['host'], $keydata['user'], $keydata['password'], $keydata['db']);
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+  }
+
+?>
