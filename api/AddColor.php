bfd3e84404b1703da0d999c3ef46caf9772af1d7
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 13:24:09 -0400

    Update to add helper functions that properly return info from database as json to callee

--- api/AddColor.php
+++ api/AddColor.php
@@ -20,10 +20,22 @@
 	else
 	{
     
-  }
+  	}
   
-  function getRequestInfo()
+  	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 ?>
