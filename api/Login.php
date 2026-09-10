efe9e607af65830cd160d190478ba377fab80ea9
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 13:08:34 -0400

    Added helper functions to decode json, create error or non error json, and return json to callee

--- api/Login.php
+++ api/Login.php
@@ -16,6 +16,29 @@
 	else
 	{
     
-  }
+    }
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
 
 ?>
