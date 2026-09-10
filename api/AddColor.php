ab7d6fed0f4aca7f2c4f9e7dc2ad7b5c340e250d
Thomas Szubka II <156004808+twothomasS@users.noreply.github.com>
2026-09-10 13:22:49 -0400

    Added the ability to decode key json and input json from callee and connect to database

--- api/AddColor.php
+++ api/AddColor.php
@@ -1,1 +1,29 @@
+<?php
+	
+	$keyjson = file_get_contents('key.json');
+	$keydata = json_decode($keyjson, true);
 
+	
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	
+	
+	$conn = new mysqli($keydata['host'], $keydata['user'], $keydata['password'], $keydata['db']);
+	if ($conn->connect_error) 
+	{
+		
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+    
+  }
+  
+  function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+?>
